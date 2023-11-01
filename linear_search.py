33cf88a3acfe3df01f4252f1f5b9a3f5c61f2976
Iliass <77488029+EILIASS@users.noreply.github.com>
2023-11-01 11:43:01 +0100

    Add files via upload

---
+++ linear_search.py
@@ -0,0 +1,21 @@
+
+# First Try
+# my first code without help
+
+# list = [5, 6, 9, 0, 2, 3, 4]
+
+# for i in list:
+#     list.sort()
+# print(list * 3)
+
+
+# Second Try
+list2 = [0, 2, 3, 4, 5, 6, 9]
+
+list3 = list2[-3::]
+list4 = list2[:4]
+print(list3)
+print(list4)
+final_list = list3 + list4
+print(final_list)
+
